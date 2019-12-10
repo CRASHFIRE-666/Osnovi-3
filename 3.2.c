2f6c6060a77091d4ceb1ad0a9627a7a61c348edc
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-10 21:51:53 +0300

    Create 3.2.c

---
+++ 3.2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	char str[1000];	
+	int strn[256] = {0};
+	int b;
+
+	gets(str);
+	puts(str);
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		b = (int)str[i];
+		strn[b] += 1;
+	}
+
+	char f;
+	for (int i = 0; i < 256; i++)
+	{
+		if (strn[i] != 0)
+		{
+			f = (char)i;
+			printf("%c = %d\n", f, strn[i]);
+		}
+	}
+
+	return 0;
+}
